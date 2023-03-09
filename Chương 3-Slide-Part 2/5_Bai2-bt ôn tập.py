051857e7b700e6a3e0e410aeb06c8b5c265af8df
Truongvangiabao <teotibin@gmail.com>
2023-03-09 10:38:35 +0700

    update

--- Chương 3-Slide-Part 2/5_Bai2-bt ôn tập.py
+++ Chương 3-Slide-Part 2/5_Bai2-bt ôn tập.py
@@ -1,22 +1,23 @@
-n=int(input('n='))
-if 2<=n<=100 :
-    if n%2==0 or n%3==0 or n%5==0 or n%7==0 :
-        print(n,'khong la SNT')
-    elif n==2 or n==3 or n==5 or n==7 :
-        print(n,'la SNT')
-    else :
-        print(n,'la SNT')
-else :
-    print('Khong hop le.')
+# n=int(input('n='))
+# if 2<=n<=100 :
+#     if n%2==0 or n%3==0 or n%5==0 or n%7==0 :
+#         print(n,'khong la SNT')
+#     elif n==2 or n==3 or n==5 or n==7 :
+#         print(n,'la SNT')
+#     else :
+#         print(n,'la SNT')
+# else :
+#     print('Khong hop le.')
 
 
             
-# s=0
-# n=int(input('n='))
-# for i in range(1,n+1):
-#     if n%i==0:
-#         s=s+1
-# if s==2:
-#     print(n,"la SNT")
-# else:
-#     print(n,'khong la SNT')
+s=0
+i=0
+n=int(input('n='))
+for i in range(1,n+1):
+    if n%i==0:
+        s=s+1
+if s==2:
+    print(n,"la SNT")
+else:
+    print(n,'khong la SNT')
