81f05c645efb8c60413d8c9d0b401fcbd029fa94
Truongvangiabao <teotibin@gmail.com>
2023-03-10 07:50:42 +0700

    update

--- Chương 3-Slide-Part 2/5_3.3.py
+++ Chương 3-Slide-Part 2/5_3.3.py
@@ -12,7 +12,7 @@
 a=1
 while a<=9 :
         for i in range (1,10) :
-                print(a*i,end=" ")
+                print(a*i,end="\t")
         if (a*i)%9==0 :
                 print("")
         a=a+1
