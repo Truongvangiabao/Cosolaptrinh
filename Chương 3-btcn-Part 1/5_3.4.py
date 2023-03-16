17696277a5bfdcdad04bebbc070321fc83fea653
Truongvangiabao <teotibin@gmail.com>
2023-03-17 01:38:24 +0700

    update

--- Chương 3-btcn-Part 1/5_3.4.py
+++ Chương 3-btcn-Part 1/5_3.4.py
@@ -3,16 +3,16 @@ b=int(input()) # lý
 c=int(input()) # hóa
 dtb=(a*2+b*3+c)/6
 if (dtb<3) :
-    print('Kém')
-if (3<=dtb<5) :
-    print('Yếu')
-if (5<=dtb<6) :
-    print('Trung bình')
-if (6<=dtb<7) :
-    print('Trung bình khá')
-if (7<=dtb<8) :
-    print('Khá')
-if (8<=dtb<9) :
-    print('Giỏi')
-if (9<=dtb<10) :
-    print('Xuất sắc')
+    print('Kem')
+elif (3<=dtb<5) :
+    print('Yeu')
+elif (5<=dtb<6) :
+    print('Trung binh')
+elif (6<=dtb<7) :
+    print('Trung binh kha')
+elif (7<=dtb<8) :
+    print('Kha')
+elif (8<=dtb<9) :
+    print('Gioi')
+elif (9<=dtb<10) :
+    print('Xuat sac')
---
+++ Luyện code/Lenh moi.py
@@ -0,0 +1,12 @@
+# a,b=input().split()  # nhập giá trị nằm ngang
+# a=int(a)
+# b=int(b)
+
+# print("{:.x1f}".format(x2)) #x1 là làm tròn đến bao nhiêu, #2 là biểu thức.
+
+
+# print(f"users setClock \{b}\{a}\{c}") # viết như bình thường, biến cho vào dấu {}
+
+# factorial: lấy giây thừa #math.factorial(5) --> 5!
+
+# a,b,c = map(int,input().split())
